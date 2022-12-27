f038343d9f9c103fcef98a891260bf132ed0d3ad
Walid VM <walid.hanifi707@gmail.com>
2022-12-27 21:27:16 +1000

    Add crud actions for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,155 +17,119 @@ const users_page = async (req, res, next) => {
   }
 }
 
-// // CREATE
-// const game_create_get = async (req, res) => {
-//   try {
-//     const platform_list = await Platform.find().populate();
-//     res.render('gameform', { // Set placeholder values in create form
-//       type: 'Create',
-//       action:'/game/create',
-//       title: 'Sample Game',
-//       summary: 'This is an example of a summary.',
-//       edition: 'Standard Edition',
-//       review: '66',
-//       price: '69.99',
-//       discount: '4.99',
-//       release: '2015-03-25',
-//       platform_list: platform_list,
-//     });
-//   } catch(err) {
-//     console.error(err);
-//     res.redirect('error', err);
-//   }   
-// }
-// const game_create_post = [
-//   // Validate & sanitize
-//   body('title').isLength({ min: 1 }).withMessage('Game name must be at least 1 character'),
-//   body('summary').optional({checkFalsy:true}),
-//   body('edition', 'Edition must not be empty.'),
-//   body('review').isInt({ min: 0, max: 100 }).withMessage('Review must be integer from 0 to 100'),
-//   body('discount').optional({checkFalsy:true}).isFloat({min: 0}).withMessage('Discount must be greater than $0'),
-//   body('release', 'Release date must not be empty').isDate().withMessage('Not a valid date'),
-//   body('platform', 'Platform must not be empty'),
+// CREATE
+const user_create_get = async (req, res) => {
+  try {
+    res.render('userform', { // Set placeholder values in create form
+      type: 'Create',
+      action:'/user/create',
+      usernmae: 'John Doe',
+      password: '',
+    });
+  } catch(err) {
+    console.error(err);
+    res.redirect('error', err);
+  }   
+}
+const user_create_post = [
+  // Validate & sanitize
+  body('username').isLength({ min: 1 }).withMessage('username must be at least 1 character'),
+  body('password').optional({checkFalsy:true}),
   
-//   // Run async callback
-//   async (req, res, next) => {
-//     const errors = validationResult(req); // Capture any validation errors parsed above
-//     if (!errors.isEmpty()) {
-//       console.error(errors.array());
-//       res.send('Validation Error: '+errors.array()[0].msg);
-//     } else {
-//       try {
-//         const newGame = await Game.create({
-//           title: req.body.title,
-//           summary: req.body.summary,
-//           edition: req.body.edition,
-//           review: req.body.review,
-//           price: req.body.price,
-//           discount: req.body.discount,
-//           release: req.body.release,
-//           platform: req.body.platform,
-//         })
-//         console.log('Game created! ('+newGame+')');
-//         res.redirect('/');
-//       } catch(err) {
-//         console.error(err);
-//         res.redirect('error', err);
-//       }
-//     }
-//   }
-// ]
+  // Run async callback
+  async (req, res, next) => {
+    const errors = validationResult(req); // Capture any validation errors parsed above
+    if (!errors.isEmpty()) {
+      console.error(errors.array());
+      res.send('Validation Error: '+errors.array()[0].msg);
+    } else {
+      try {
+        const newuser = await User.create({
+          username: req.body.username,
+          password: req.body.password,
+        })
+        console.log('user created! ('+newuser+')');
+        res.redirect('/');
+      } catch(err) {
+        console.error(err);
+        res.redirect('error', err);
+      }
+    }
+  }
+]
 
-// // READ
-// const game_read = async (req, res) => {
-//   try {
-//     const foundgame = await Game.findById(req.params.id).populate('platform');
-//     const platform_list = await Platform.find().populate();
-//     res.render('gameform', {
-//       type: 'Update',
-//       action:'/game/'+foundgame.id,
-//       platform_list: platform_list,
-//       title: foundgame.title,
-//       summary: foundgame.summary,
-//       edition: foundgame.edition,
-//       review: foundgame.review,
-//       price: foundgame.price,
-//       discount: foundgame.discount,
-//       release: foundgame.release,
-//     })
-//   } catch(err) {
-//     console.error(err);
-//     res.redirect('error', err);
-//   }
-// }
+// READ
+const user_read = async (req, res) => {
+  try {
+    const founduser = await User.findById(req.params.id);
+    res.render('userform', {
+      type: 'Update',
+      action:'/user/'+foundUser.id,
+      username: foundUser.username,
+      password: foundUser.password,
+    })
+  } catch(err) {
+    console.error(err);
+    res.redirect('error', err);
+  }
+}
 
-// // UPDATE
-// const game_update = [
-//   // Validate & sanitize
-//   body('title').isLength({ min: 1 }).withMessage('Game name must be at least 1 character'),
-//   body('summary').optional({checkFalsy:true}),
-//   body('edition', 'Edition must not be empty.'),
-//   body('review').isInt({ min: 0, max: 100 }).withMessage('Review must be integer from 0 to 100'),
-//   body('discount').optional({checkFalsy:true}).isFloat({min: 0}).withMessage('Discount must be greater than $0'),
-//   body('release', 'Release date must not be empty').isDate().withMessage('Not a valid date'),
-//   body('platform', 'Platform must not be empty'),
+// UPDATE
+const user_update = [
+  // Validate & sanitize
+  body('username').isLength({ min: 1 }).withMessage('username must be at least 1 character'),
+  body('password').optional({checkFalsy:true}),
 
-//   // Run async callback
-//   async (req, res) => {
-//     const errors = validationResult(req); // Capture any validation errors parsed above
-//     if (!errors.isEmpty()) {
-//       console.error(errors.array());
-//       res.send('Validation Error: '+errors.array()[0].msg);
-//     } else {
-//       try {
-//         const newGame = await Game.findByIdAndUpdate(
-//           { _id: req.params.id },
-//           {
-//           title: req.body.title,
-//           summary: req.body.summary,
-//           edition: req.body.edition,
-//           review: req.body.review,
-//           price: req.body.price,
-//           discount: req.body.discount,
-//           release: req.body.release,
-//           platform: req.body.platform}
-//         )
-//         console.log('Game updated! ('+newGame+')');
-//         res.redirect('/games');
-//       } catch(err) {
-//         console.error(err);
-//         res.redirect('error', err);
-//       }
-//     }
-//   }
-// ]
+  // Run async callback
+  async (req, res) => {
+    const errors = validationResult(req); // Capture any validation errors parsed above
+    if (!errors.isEmpty()) {
+      console.error(errors.array());
+      res.send('Validation Error: '+errors.array()[0].msg);
+    } else {
+      try {
+        const newuser = await User.findByIdAndUpdate(
+          { _id: req.params.id },
+          {
+            username: req.body.username,
+            password: req.body.password}
+        )
+        console.log('user updated! ('+newuser+')');
+        res.redirect('/allusers');
+      } catch(err) {
+        console.error(err);
+        res.redirect('error', err);
+      }
+    }
+  }
+]
 
-// // DESTROY
-// const game_destroy_get = async (req, res) => {
-//   try {
-//     res.render('delete');
-//   } catch(err) {
-//     console.error(err);
-//     res.redirect('error', err);
-//   }
-// }
-// const game_destroy_post = async (req, res) => {
-//     try {
-//         const foundGame = await Game.findByIdAndDelete(req.params.id);
-//         console.log('Game deleted! ('+foundGame+')');
-//         res.redirect('/games');
-//     } catch(err) {
-//         console.error(err);
-//         res.redirect('error', err);
-//     }
-// }
+// DESTROY
+const user_destroy_get = async (req, res) => {
+  try {
+    res.render('delete');
+  } catch(err) {
+    console.error(err);
+    res.redirect('error', err);
+  }
+}
+const user_destroy_post = async (req, res) => {
+    try {
+        const founduser = await User.findByIdAndDelete(req.params.id);
+        console.log('user deleted! ('+founduser+')');
+        res.redirect('/allusers');
+    } catch(err) {
+        console.error(err);
+        res.redirect('error', err);
+    }
+}
 
 module.exports = {
     users_page,
-    // game_create_get,
-    // game_create_post,
-    // game_read,
-    // game_update,
-    // game_destroy_get,
-    // game_destroy_post,
+    user_create_get,
+    user_create_post,
+    user_read,
+    user_update,
+    user_destroy_get,
+    user_destroy_post,
 }
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -4,9 +4,19 @@ const router = express.Router();
 // Require controllers
 const userController = require('../controllers/userController');
 
-//homepage
+// SHOW ALL
 router.get('/allusers', userController.users_page); // render page of all users
+// CREATE
+router.get('/user/create', userController.user_create_get); // redirect to user create form
+router.post('/user/create', userController.user_create_post);
+// READ
+router.get('/user/:id', userController.user_read);
+// UPDATE
+router.post('/user/:id', userController.user_update);
+// DESTROY
+router.get('/user/:id/delete', userController.user_destroy_get); // redirect to delete page, asking to confirm deletion
+router.post('/user/:id/delete', userController.user_destroy_post); // process delete.js submit button
 
-// All other routes
+// Post Routes
 
 module.exports = router;
