c0cc0ae02fa5d96b7fd6be695aad4cc698ef8af1
Walid VM <walid.hanifi707@gmail.com>
2022-12-29 17:32:27 +1000

    Add required field for names

--- models/user.js
+++ models/user.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    membership: { type: Boolean, required: true },
+    membership: { type: Boolean, default: false },
 })
 
 // Export model
