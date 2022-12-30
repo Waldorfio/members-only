317e72293ab3320fd3d0416fc3f6bbef956cd4d4
Walid VM <walid.hanifi707@gmail.com>
2022-12-30 21:09:47 +1000

    Pass through messages variable to index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/message');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { user: req.user });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { user: req.user });
+// });
+
+router.get('/', async (req, res, next) => {
+  try {
+    const msg_list = await Message.find();
+    res.render('index', {
+      user: req.user,
+      messages: msg_list,
+    });
+  } catch(err) {
+    console.error(err);
+    res.redirect('error', err);
+  }
+})
 
 module.exports = router;
